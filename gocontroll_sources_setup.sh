f0ddf046329e735126d1cb0f7d156d7fa9edff86
Maud Spierings <maud_spierings@hotmail.com>
2025-01-14 13:35:01 +0100

    first commit for bookworm

---
+++ gocontroll_sources_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p /usr/share/keyrings
+
+rm -f /usr/share/keyrings/gocontroll.gpg || true
+rm -f /etc/apt/sources.list.d/gocontroll.list || true
+
+curl -fsSL http://server.gocontroll.com/gpg-pubkey.asc | gpg --dearmor -o /usr/share/keyrings/gocontroll.gpg
+
+chmod 644 /usr/share/keyrings/gocontroll.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/gocontroll.gpg] http://server.gocontroll.com/ bookworm main" | tee /etc/apt/sources.list.d/gocontroll.list > /dev/null
+
+apt-get update -y
